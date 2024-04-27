7840c26c4088a653141eba0b9dc8f179c5e46c1b
VIKASH KUMAR <mevikash606@gmail.com>
2024-04-27 16:30:04 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+module "google_api_gateway_api" {
+  source = "./Api_Gateway"
+  project_id =  var.project_id
+  region = var.region
+  api_config_id_prefix = var.api_config_id_prefix
+  api_gateway_container_id = var.api_gateway_container_id
+  gateway_id = var.gateway_id
+  api_gw_display_name = var.api_gw_display_name
+  api_cfg_display_name = var.api_cfg_display_name
+  gw_display_name = var.gw_display_name
+
+}
---
+++ terraform.tfvars
@@ -0,0 +1,12 @@
+# project_id       = "ceq-devops-sandbox"
+
+project_id = "ceq-devops-sandbox"
+region = "europe-west1"
+api_config_id_prefix = "api"
+api_gateway_container_id = "api-gw"
+gateway_id = "gw"
+
+
+api_gw_display_name = "GCP-POD_API Gateway"
+api_cfg_display_name = "GCP-POD_The Config"
+gw_display_name = "GCP-POD_The Gateway"
